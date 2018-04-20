dbcb7835583c117b148137f7f8c745f243f1fecd
Jeroen Van Bruyssel <jeroen.vb1@gmail.com>
2018-04-20 17:21:02 +0200

    Better solution

--- 18_maxpathsum_2.py
+++ 18_maxpathsum_2.py
@@ -2,42 +2,25 @@
 # Find the maximum total from top to bottom of the triangle below
 
 import logging
-import pprint
-import copy
+import time
 
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+logging.basicConfig(level=logging.CRITICAL, format=' %(asctime)s - %(levelname)s- %(message)s')
 
 
 def txtreader(filename):
     text_file = open(filename + '.txt', 'r')
-    lines = text_file.read().split('\n')
-    grid = []
-    for i in range(len(lines)):
-        grid.append(list(map(int, lines[i].split(' '))))
-    text_file.close()
-    return grid
+    lines = text_file.readlines()
+    for l in range(len(lines)):
+        line = lines[l].rstrip()
+        lines[l] = [int(i) for i in line.split(" ")]
+    return lines
 
+lines = txtreader('18_maxpathsum_2')
+start = time.time()
+sums = lines[len(lines) - 1]
+for i in range(len(lines) - 2, -1, -1):
+    for j in range(len(lines[i])):
+        sums[j] = max([lines[i][j] + sums[j], lines[i][j] + sums[j + 1]])
+duration = time.time() - start
 
-pyr = txtreader('18_maxpathsum_2')
-
-lastsums = {(0, 0): pyr[0][0]}
-for i in range(len(pyr) - 1):
-    currentsums = {}
-    for item in lastsums.items():
-        coord = item[0]
-        lastsum = item[1]
-        logging.debug('%d at %s' % (lastsum, coord))
-        coordL = (coord[0] + 1, coord[1])
-        nextsumL = lastsum + pyr[coordL[0]][coordL[1]]
-        coordR = (coord[0] + 1, coord[1] + 1)
-        nextsumR = lastsum + pyr[coordR[0]][coordR[1]]
-        logging.debug('Left: %d     Right: %d' % (nextsumL, nextsumR))
-        oldL = currentsums.setdefault(coordL, 0)
-        oldR = currentsums.setdefault(coordR, 0)
-        currentsums[coordL] = max(nextsumL, oldL)
-        logging.debug('Added %d' % currentsums[coordL])
-        currentsums[coordR] = max(nextsumR, oldR)
-        logging.debug('Added %d' % currentsums[coordR])
-    lastsums = copy.deepcopy(currentsums)
-
-print(max(lastsums.values()))
+print("Max sum: %d - duration: %0.4fs" % (sums[0], duration))
