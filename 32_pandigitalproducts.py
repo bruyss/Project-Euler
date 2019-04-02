e1d8f51c4ab4de9afd32a3a03203a8468463a88e
Jeroen Van Bruyssel <jeroen.vb1@gmail.com>
2019-04-02 15:30:58 +0200

    Problem 32 supa speed

--- 32_pandigitalproducts.py
+++ 32_pandigitalproducts.py
@@ -5,31 +5,16 @@
 # product is 1 through 9 pandigital.
 # Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
 # HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
-
-from math import log10 as log
-from math import floor
-
-
-def getdigits(num):
-    digits = []
-    while num > 0:
-        digits.insert(0, num % 10)
-        num = num // 10
-    return digits
 	
 
 def is_panprod(ma, mp):
 	prod = ma * mp
-	digits = getdigits(ma) + getdigits(mp) + getdigits(prod)
-	digits.sort()
-	if digits == list(range(1, 10)):
+	digits = set(str(ma) + str(mp) + str(prod)) - {'0'}
+	# print(digits)
+	if len(digits) == 9:
 		return True
 	else:
 		return False
-		
-
-def flog(x):
-	return floor(log(x))
 	
 
 pandigital_products = set([])
@@ -37,9 +22,10 @@ assert(is_panprod(39, 186))
 
 for multiplicand in range(1, 10**5 + 1):
 	multiplier = multiplicand + 1
-	while flog(multiplicand) + flog(multiplier) + flog(multiplicand * multiplier) <= 9:
-		print(f'{multiplicand} x {multiplier}')
+	while len(str(multiplicand) + str(multiplier) + str(multiplicand * multiplier)) <= 9:
+		# print(f'{multiplicand} x {multiplier}')
 		if is_panprod(multiplicand, multiplier):
+			print(f"{multiplicand} x {multiplier} = {multiplicand * multiplier}")
 			pandigital_products.add(multiplicand * multiplier)
 		multiplier += 1
 
