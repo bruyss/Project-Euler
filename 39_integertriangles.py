450e0fd4325ab90fef325472304b61452b8b72db
Jeroen Van Bruyssel <jeroen.vb1@gmail.com>
2019-04-06 09:31:56 +0200

    PEP8 errors

--- 39_integertriangles.py
+++ 39_integertriangles.py
@@ -1,5 +1,5 @@
 #! python3
-# If p is the perimeter of a right angle triangle with integral length sides, 
+# If p is the perimeter of a right angle triangle with integral length sides,
 # {a,b,c}, there are exactly three solutions for p = 120.
 #
 # {20,48,52}, {24,45,51}, {30,40,50}
@@ -11,17 +11,17 @@ res_max = 0
 res_n = 0
 
 for n in range(1, n_max + 1):
-	res = 0
-	for a in range(1, n):
-		b = a
-		while a + b < n:
-			c = n - a - b
-			# print(f"({a}, {b}, {c})")
-			if a**2 + b**2 == c**2:
-				print(f"{n}: ({a}, {b}, {c})")
-				res += 1
-			b += 1		
-	if res > res_max:
-		res_max = res
-		res_n = n
-		print(f"New max {res_max} for p = {res_n}")
+    res = 0
+    for a in range(1, n):
+        b = a
+        while a + b < n:
+            c = n - a - b
+            # print(f"({a}, {b}, {c})")
+            if a**2 + b**2 == c**2:
+                print(f"{n}: ({a}, {b}, {c})")
+                res += 1
+            b += 1
+    if res > res_max:
+        res_max = res
+        res_n = n
+        print(f"New max {res_max} for p = {res_n}")
--- 40_champernowne.py
+++ 40_champernowne.py
@@ -1,9 +1,9 @@
 #! python3
-# An irrational decimal fraction is created 
+# An irrational decimal fraction is created
 # by concatenating the positive integers:
 # 0.123456789101112131415161718192021...
 # It can be seen that the 12th digit of the fractional part is 1.
-# If dn represents the nth digit of the fractional part, 
+# If dn represents the nth digit of the fractional part,
 # find the value of the following expression.
 # d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 
@@ -16,8 +16,8 @@ print(idxs)
 res = []
 
 for d in prod_digits:
-	aantal_cijfers = next(i for i, v in enumerate(idxs) if v > d)
-	onder_idx = idxs[aantal_cijfers - 1]
-	print(f"{aantal_cijfers} {onder_idx}")
-	getal_idx = (d - onder_idx + 1) // aantal_cijfers
-	print(getal_idx)
+    aantal_cijfers = next(i for i, v in enumerate(idxs) if v > d)
+    onder_idx = idxs[aantal_cijfers - 1]
+    print(f"{aantal_cijfers} {onder_idx}")
+    getal_idx = (d - onder_idx + 1) // aantal_cijfers
+    print(getal_idx)
