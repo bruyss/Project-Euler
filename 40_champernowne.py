11ffb45c90c30238a5812774714e187c74188615
Jeroen Van Bruyssel <jeroen.vb1@gmail.com>
2019-04-08 23:28:04 +0200

    Problem 40 brak

--- 40_champernowne.py
+++ 40_champernowne.py
@@ -7,23 +7,13 @@
 # find the value of the following expression.
 # d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 
-from itertools import accumulate
-# from functools import reduce
+champ = ""
+i = 1
+while len(champ) < 10**7:
+    champ += str(i)
+    i += 1
 
-prod_digits = [10**x for x in range(7)]
-idxs = list(accumulate([1] + [(9 * 10**(x - 1)) * x for x in range(1, 8)]))
-print(idxs)
-
-res = []
-
-for d in prod_digits:
-    aantal_cijfers = next(i for i, v in enumerate(idxs) if v > d)
-    onder_idx = idxs[aantal_cijfers - 1]
-    print(f"{aantal_cijfers} {onder_idx}")
-    getal_idx = (d - onder_idx) // aantal_cijfers
-    print(getal_idx)
-    getal = onder_idx + getal_idx
-    print(getal)
-    res.append(int(str(getal)[getal_idx % aantal_cijfers]))
-
+res = 1
+for i in [10**x - 1 for x in range(7)]:
+    res *= int(champ[i])
 print(res)
