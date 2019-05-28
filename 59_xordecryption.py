10b75e1b1d8646134bbb09917f39ea97055a3808
Jeroen Van Bruyssel <jeroen.vb1@gmail.com>
2019-05-28 10:17:10 +0200

    Solved problem 59

---
+++ 59_xordecryption.py
@@ -0,0 +1,73 @@
+#! python3
+# Each character on a computer is assigned a unique code and the preferred
+# standard is ASCII (American Standard Code for Information Interchange).
+# For example, uppercase A = 65, asterisk (*) = 42, and lowercase k = 107.
+
+# A modern encryption method is to take a text file, convert the bytes to ASCII
+# then XOR each byte with a given value, taken from a secret key. The advantage
+# with the XOR function is that using the same encryption key on the cipher
+# text, restores the plain text; for example, 65 XOR 42 = 107, then 107 XOR
+# 42 = 65.
+
+# For unbreakable encryption, the key is the same length as the plain text
+# message, and the key is made up of random bytes. The user would keep the
+# encrypted message and the encryption key in different locations, and without
+# both "halves", it is impossible to decrypt the message.
+
+# Unfortunately, this method is impractical for most users, so the modified
+# method is to use a password as a key. If the password is shorter than the
+# message, which is likely, the key is repeated cyclically throughout the
+# message. The balance for this method is using a sufficiently long password
+# key for security, but short enough to be memorable.
+
+# Your task has been made easy, as the encryption key consists of three lower
+# case characters. Using p059_cipher.txt (right click and 'Save Link/Target
+# As...'), a file containing the encrypted ASCII codes, and the knowledge
+# that the plain text must contain common English words, decrypt the
+# message and find the sum of the ASCII values in the original text.
+
+from itertools import (cycle, combinations_with_replacement as combrep,
+                       permutations, combinations)
+
+with open("59_text.txt", "r") as f:
+    text = f.read()
+asciicodes = list(map(lambda x: int(x), text.split(',')))
+
+lcaseletters = (chr(x) for x in range(ord('a'), ord('z') + 1))
+print(lcaseletters)
+
+lcaseascii = set(range(ord('a'), ord('z') + 1))
+ucaseascii = set(range(ord('A'), ord('Z') + 1))
+lettersascii = lcaseascii | ucaseascii
+print(lettersascii)
+
+keyscombs = map(lambda x: ''.join(x), set(combrep(lcaseletters, 3)))
+keys = set()
+for c in keyscombs:
+    perms = permutations(c)
+    for p in perms:
+        keys.add("".join(p))
+
+res = []
+mfreq = 0
+mkey = ''
+mdecoded = []
+
+for key in keys:
+    # print(key)
+    kr = cycle(key)
+    decoded = []
+    for code in asciicodes:
+        decoded.append(code ^ ord(next(kr)))
+        # print(code ^ ord(next(kr)))
+    # print(decoded)
+    nrletters = len([a for a in decoded if a in lettersascii])
+    if nrletters / len(decoded) > mfreq:
+        mfreq = nrletters / len(decoded)
+        mkey = key
+        mdecoded = decoded
+
+print(mkey)
+print(mfreq)
+print("".join(list(map(lambda x: chr(x), mdecoded))))
+print(sum(mdecoded))
