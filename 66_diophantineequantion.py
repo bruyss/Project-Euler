483894056fe1828ded9058c67f50513734ad55c9
VAN BRUYSSEL Jeroen <Jeroen.VANBRUYSSEL@veibe.net>
2020-02-21 10:37:28 +0100

    Stuck 66

--- 66_diophantineequantion.py
+++ 66_diophantineequantion.py
@@ -3,24 +3,23 @@
 # Find the value of D ≤ 1000 in minimal solutions of x for which the largest
 # value of x is obtained
 
-from math import sqrt, floor, ceil
+from math import sqrt
 
-Dlim = 1000
-xs = []
-xmax = 0
-Dmax = 0
+squares = {x**2 for x in range(1, 10**6)}
+print("squares")
+Ds = {x for x in range(1,1001) if x not in squares}
 
-squares = {x**2 for x in range(ceil(sqrt(Dlim)))}
-Ds = set(range(Dlim + 1)) - squares
+D = 98
+x = 0
 
 for D in Ds:
-    x = 1
-    y = 1
-    while x**2 - D * y**2 != 1:
+    print(f"D = {D}")
+    x = 0
+    while(True):
         x += 1
-        y = floor(sqrt((x**2 - 1) / D))
-    print(f"{x}^2 - {D} * {y}^2 = 1")
-    if x >= xmax:
-        xmax = x
-        Dmax = D
-        print(f"New max {xmax} in {Dmax}\n")
+        # print(x)
+        ysq = (x**2 - 1) / D
+        if ysq in squares:
+            y = int(sqrt(ysq))
+            print(f"x = {x}\ny = {y}\n\n")
+            break
